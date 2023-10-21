d6e4806f470b2093a24db15f76ba4d04bbdd1a0c
martimds <martimtimdias@gmail.com>
2023-10-21 19:34:24 -0300

    Alterado navbar

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/quemsomos')
-def quemsomos():
-    return render_template('quemsomos.html')
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
